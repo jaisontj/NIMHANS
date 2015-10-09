67f57e11319c2cdb5e9c91a9b79a38910d17c716
JAISON <jaisontj92@gmail.com>
2015-10-09 11:55:50 +0530

    Page 1 layout done

--- NIMHANS/PatientListViewController.swift
+++ NIMHANS/PatientListViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 import Foundation
 import CoreData
 
-class PatientListViewController: UIViewController {
-
+class PatientListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    @IBAction func logOutClicked(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func addPatientClicked(sender: UIBarButtonItem) {
+        self.performSegueWithIdentifier("showsPatientDetails", sender: nil)
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("patientName", forIndexPath: indexPath) as UITableViewCell
+        return cell
+    }
+    
+    
 }
